58d5ceff78ff72462cbb946f2b014bf5aaca49e3
Eric Hay <erichay@live.ca>
2026-05-29 13:35:00 -0700

    application: Add minimal applications for each core
    Add two minimal example applications that can be built for each core. Both
    applications expect semihosting to be enabled and will output an incrementing
    sequence of numbers over the console.
    
    Signed-off-by: Eric Hay <erichay@live.ca>

---
+++ core_0/src/main.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ * Copyright (c) 2026 Eric Hay
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+static int i = 0;
+
+int main(void)
+{
+	printk("Hello World from Core 0!\n");
+
+	while (1) {
+		k_msleep(1000);
+		printk("Running main loop iteration %d\n", i++);
+	}
+
+	return 0;
+}
