dfefd37295d15c4f273c7860d0808fa918cf78a4
Eric Hay <erichay@live.ca>
2026-06-02 17:52:51 -0700

    ipm: Enable interprocesor communication
    Demonstrate IPM across applications and cores with the IPM driver
    and the RP2040 MBOX driver.
    
    Signed-off-by: Eric Hay <erichay@live.ca>

--- core_0/src/main.c
+++ core_0/src/main.c
@@ -6,16 +6,47 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/ipm.h>
 
 static int i = 0;
 
+static const struct device *ipm_dev;
+
+static void ipm_callback(const struct device *dev, void *context, uint32_t id, volatile void *data)
+{
+	int received_value = *(int *)data;
+	printk("Received IPM message with value: %d\n", received_value);
+}
+
 int main(void)
 {
+	int status;
+
 	printk("Hello World from Core 0!\n");
 
+	ipm_dev = DEVICE_DT_GET(DT_NODELABEL(ipm_mbox0));
+	if (!ipm_dev) {
+		printk("Failed to get IPM device\n");
+		return -1;
+	}
+
+	ipm_register_callback(ipm_dev, ipm_callback, NULL);
+
+	status = ipm_set_enabled(ipm_dev, 1);
+	if (status != 0) {
+		printk("Failed to enable IPM device\n");
+		return -1;
+	}
+
 	while (1) {
-		k_msleep(1000);
+		k_msleep(5000);
 		printk("Running main loop iteration %d\n", i++);
+
+		status = ipm_send(ipm_dev, -1, 0, &i, sizeof(i));
+		if (status != 0) {
+			printk("Failed to send IPM message\n");
+			return -1;
+		}
 	}
 
 	return 0;
--- core_1/src/main.c
+++ core_1/src/main.c
@@ -6,15 +6,40 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/ipm.h>
 
 static int i = 0;
 
+static const struct device *ipm_dev;
+
+static void ipm_callback(const struct device *dev, void *context, uint32_t id, volatile void *data)
+{
+	int received_value = *(int *)data;
+	printk("Received IPM message with value: %d\n", received_value);
+}
+
 int main(void)
 {
+	int status;
+
 	printk("Hello World from Core 1!\n");
 
+	ipm_dev = DEVICE_DT_GET(DT_NODELABEL(ipm_mbox1));
+	if (!ipm_dev) {
+		printk("Failed to get IPM device\n");
+		return -1;
+	}
+
+	ipm_register_callback(ipm_dev, ipm_callback, NULL);
+
+	status = ipm_set_enabled(ipm_dev, 1);
+	if (status != 0) {
+		printk("Failed to enable IPM device\n");
+		return -1;
+	}
+
 	while (1) {
-		k_msleep(1000);
+		k_msleep(10000);
 		printk("Running main loop iteration %d\n", i++);
 	}
 
